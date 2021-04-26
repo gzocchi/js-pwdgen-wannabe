111d7a47c846ce059ab7c8ceef0253032012aecd
gzocchi <z.giorgio.z@gmail.com>
2021-04-26 19:42:55 +0200

    modificato 21 in numero casuale (da 1 a 100)

--- js/script.js
+++ js/script.js
@@ -12,6 +12,7 @@ console.log("l'utente ha scritto = " + " " + nomeUtente + " " + cognomeUtente + 
 var nome = nomeUtente.toLowerCase();
 var cognome = cognomeUtente.toLowerCase();
 var colore = coloreUtente.toLowerCase();
+var numeroCasuale = Math.floor(Math.random() * 100) + 1;
 
 // debug
 console.log("Testo modificato: da " + nomeUtente + " a " + nome);
@@ -19,4 +20,4 @@ console.log("Testo modificato: da " + cognomeUtente + " a " + cognome);
 console.log("Testo modificato: da " + coloreUtente + " a " + colore);
 
 // Scrivi sulla pagina nomecognomecolorepreferito21
-document.getElementById("personal_pwd").innerHTML = nome + cognome + colore + "21"
+document.getElementById("personal_pwd").innerHTML = nome + cognome + colore + numeroCasuale
