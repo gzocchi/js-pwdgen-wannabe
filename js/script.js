d9d2fdb4121ac14b8288d63fbcc5480109a0b9b1
gzocchi <z.giorgio.z@gmail.com>
2021-04-26 14:00:13 +0200

    modoficato script generazione pwd

--- js/script.js
+++ js/script.js
@@ -1,14 +1,22 @@
 // Chiedi all'utente il suo nome
 var nomeUtente = prompt("Ciao, qual'è il tuo nome?")
 // Chiedi il suo cognome
-var cognomeUtente = prompt("Ora scrivi il tuo cognome!")
+var cognomeUtente = prompt("Ora scrivimi il tuo cognome!")
 // Chiedi il suo colore preferito
-var coloreUtente = prompt("ora scrivi il tuo cognome!")
+var coloreUtente = prompt("Scrivi il tuo colore preferito.")
 
 // debug
-console.log(nomeUtente);
-console.log(cognomeUtente);
-console.log(coloreUtente);
+console.log("l'utente ha scritto = " + " " + nomeUtente + " " + cognomeUtente + " " + coloreUtente);
+
+
+var nome = nomeUtente.toLowerCase();
+var cognome = cognomeUtente.toLowerCase();
+var colore = coloreUtente.toLowerCase();
+
+// debug
+console.log("Testo modificato: da " + nomeUtente + " a " + nome);
+console.log("Testo modificato: da " + cognomeUtente + " a " + cognome);
+console.log("Testo modificato: da " + coloreUtente + " a " + colore);
 
 // Scrivi sulla pagina nomecognomecolorepreferito21
-document.getElementById("personal_pwd").innerHTML = nomeUtente + cognomeUtente + coloreUtente + "21"
+document.getElementById("personal_pwd").innerHTML = nome + cognome + colore + "21"
